bee6b5abcded5fd3ab364e6a4e3506e398093394
Ednan Jhony <ednanjhony@yahoo.com.br>
2020-06-16 05:56:11 -0300

    token

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,43 @@
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IUsersTokensRepository from '../repositories/IUsersTokensRepository';
+
+// import User from '../infra/typeorm/entities/User';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+
+    @inject('UsersTokensRepository')
+    private userTokensRepository: IUsersTokensRepository,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exists.');
+    }
+
+    const { token } = await this.userTokensRepository.generate(user.id);
+
+    await this.mailProvider.sendMail(
+      email,
+      `Pedido de Recuperaçao de senha recebido: ${token}`,
+    );
+  }
+}
+
+export default SendForgotPasswordEmailService;
